a9b06d27cd6a9dcf36851feae21dad58b262b4b4
Patrick <patcodes@posteo.ca>
2022-08-15 21:39:33 -0400

    added ol-ext, id logic, zoom logic

--- src/app/Models/Coord.ts
+++ src/app/Models/Coord.ts
@@ -2,10 +2,9 @@ export class Coord {
   latitude: number;
   longitude: number;
 
-  constructor(lattitude?: number, longitude?: number) {
-    this.latitude = lattitude || 0;
+  constructor(longitude?: number, latitude?: number) {
+    this.latitude = latitude || 0;
     this.longitude = longitude || 0;
-
   }
 
 
